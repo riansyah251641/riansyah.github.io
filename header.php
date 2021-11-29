e240df2e14fd4e9a03e1205bdfd5e2345920a499
riansyah251641 <ggrhimo1641@gmail.com>
2021-11-29 12:45:50 +0700

    pertama commit

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <style>
+        * {
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+
+        .top {
+            position: absolute;
+            line-height: 80px;
+            background-color: aqua;
+            width: 1260px;
+            overflow-x: hidden;
+        }
+
+        ul {
+            display: flex;
+            text-decoration: none;
+        }
+
+        li {
+            padding: 15px 20px;
+            font-size: large;
+            text-decoration: none;
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="top">
+        <table>
+            <ul>
+                <li>HOME</li>
+                <li>CLASS</li>
+                <li>BACK</li>
+            </ul>
+        </table>
+    </div>
