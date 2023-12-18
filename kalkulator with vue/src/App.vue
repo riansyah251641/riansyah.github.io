b909d39f8701ca83e2a4046a140b470950967e00
rian syah <91471268+riansyah251641@users.noreply.github.com>
2023-12-18 21:29:12 +0700

    Add files via upload

---
+++ kalkulator with vue/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Kalkulator Sederhana</h1>
+    <p>Hasil: {{ hasil }}</p>
+    <input v-model="angka1" type="number" placeholder="Angka pertama" />
+    <input v-model="angka2" type="number" placeholder="Angka kedua" />
+    <br /><br />
+    <button @click="add">+</button>
+    <button @click="subtract">-</button>
+    <button @click="multiply">*</button>
+    <button @click="divide">/</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      angka1: 0,
+      angka2: 0,
+      hasil: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.hasil = this.angka1 + this.angka2;
+    },
+    subtract() {
+      this.hasil = this.angka1 - this.angka2;
+    },
+    multiply() {
+      this.hasil = this.angka1 * this.angka2;
+    },
+    divide() {
+      this.hasil = this.angka1 / this.angka2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add scoped styles here if needed */
+</style>
